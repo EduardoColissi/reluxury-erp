d6eff4c072d74da2f2af4d8773e44b4ab309fab4
Eduardo Colissi <colissi.eduardo@gmail.com>
2024-08-07 22:24:38 -0300

    refactor: changed some initial configurations and connected database

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,4 @@
+<h3>Dashboard renderizado pela view</h3>
+{{-- coMEN --}}
+<?= 'TESTE' ?>
+<?php echo 'TESTE' ?>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Página de login!';
+});
+
+Route::get('/signup', function () {
+    return 'Página de cadastro!';
+});
+
+Route::get('/login', function () {
+    echo 'Login';
+});
+
+Route::prefix('app')->group(function () {
+    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index']);
+});
+
+Route::fallback(function () { 
+    echo 'A rota acessada não existe. <a href="/">Clique aqui</a> para voltar para a página inicial.';  
 });
